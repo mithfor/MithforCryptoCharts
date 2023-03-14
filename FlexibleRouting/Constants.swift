0cc3edc05aa8610b33039aca14af8cbd35721c8c
Dmitriy Voronin <devoronin@gmail.com>
2023-03-14 17:38:01 +0300

    Constants were added

---
+++ FlexibleRouting/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  FlexibleRouting
+//
+//  Created by Dmitrii Voronin on 14.03.2023.
+//
+
+import Foundation
+
+struct TitlesConstants {
+    static let assets = "Assets"
+    static let watchlist = "Watchlist"
+    static let settings = "Settings"
+}
