f816c9426a593e970808515c96ece22e769e9902
Dmitriy Voronin <devoronin@gmail.com>
2024-11-14 16:34:56 +0300

    Add NetworkingManager.handleURLResponse(output:)

--- MithforCryptoCharts/SwiftUI/Utilities/NetworkingManager.swift
+++ MithforCryptoCharts/SwiftUI/Utilities/NetworkingManager.swift
@@ -13,18 +13,19 @@ class NetworkingManager {
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { output in
-                
-                guard let response = output.response as? HTTPURLResponse,
-                      isSuccessfull(statusCode: response.statusCode) == true
-                else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                return output.data
-            }
+            .tryMap({ try handleURLResponse(output: $0) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
+    }
+    
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse,
+              isSuccessfull(statusCode: response.statusCode) == true
+        else {
+            throw URLError(.badServerResponse)
+        }
+        
+        return output.data
         
         func isSuccessfull(statusCode: Int) -> Bool {
             return (statusCode >= 200 && statusCode < 300) ? true : false
