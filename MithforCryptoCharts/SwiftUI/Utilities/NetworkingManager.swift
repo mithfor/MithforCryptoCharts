6634b4b789547dce5369e6e7c2160e6183257920
Dmitriy Voronin <devoronin@gmail.com>
2024-11-14 16:53:57 +0300

    Add a reusable Networking layer

--- MithforCryptoCharts/SwiftUI/Utilities/NetworkingManager.swift
+++ MithforCryptoCharts/SwiftUI/Utilities/NetworkingManager.swift
@@ -10,19 +10,34 @@ import Combine
 
 class NetworkingManager {
     
+    enum NetworkingError: LocalizedError {
+        case badURLResponse(url: URL)
+        case unknown
+        
+        var errorDescription: String? {
+            switch self {
+            case .badURLResponse(let url):
+                return "[🔥] Bad URL response. \(url)"
+            case .unknown:
+                return "[⚠️] Unknown error occured"
+            }
+        }
+    }
+    
     static func download(url: URL) -> AnyPublisher<Data, Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap({ try handleURLResponse(output: $0) })
+            .tryMap({ try handleURLResponse(output: $0, url: url) })
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        
         guard let response = output.response as? HTTPURLResponse,
               isSuccessfull(statusCode: response.statusCode) == true
         else {
-            throw URLError(.badServerResponse)
+            throw NetworkingError.badURLResponse(url: url)
         }
         
         return output.data
