a5aff2fe9063b14d4b540653a75969df27fb7de8
Dmitriy Voronin <devoronin@gmail.com>
2024-11-14 16:25:06 +0300

    Add NetworkingManager.handleCompletion on sink

--- MithforCryptoCharts/SwiftUI/Services/CoinDataService.swift
+++ MithforCryptoCharts/SwiftUI/Services/CoinDataService.swift
@@ -20,50 +20,15 @@ class CoinDataService {
         
         guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&ids=bitcoin&category=layer-1&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
         
-        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
-            .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { [weak self] output in
-                
-                guard let response = output.response as? HTTPURLResponse,
-                      self?.isSuccessfull(statusCode: response.statusCode) == true
-                else {
-                    throw URLError(.badServerResponse)
-                }
-                
-                return output.data
-            }
-            .receive(on: DispatchQueue.main)
-//            .decode(
-//                type: [CoinModel].self,
-//                decoder: JSONDecoder()
-//            )
-            .tryMap({ data in
-                let decoder = JSONDecoder()
-                do {
-                    print(data)
-                    return  try decoder.decode([CoinModel].self, from: data)
-                } catch let error as DecodingError {
-                    print("\(#function) : \(NetworkError.unableToDecode.rawValue)")
-                    print("DecodingError: \(error)")
-                    throw NetworkError.unableToDecode
-                }
-            })
-
-            .sink { (completion) in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("\(#function) : \(error.localizedDescription)")
-                }
-            } receiveValue: { [weak self] returnedCoins in
-                guard let self = self else { return }
+        coinSubscription = NetworkingManager.download(url: url)
+            .decode(
+                type: [CoinModel].self,
+                decoder: JSONDecoder()
+            )
+            .sink(receiveCompletion: NetworkingManager.handleCompletion,
+                  receiveValue: { returnedCoins in
                 self.allCoins = returnedCoins
                 self.coinSubscription?.cancel()
-            }
-    }
-    
-    private func isSuccessfull(statusCode: Int) -> Bool {
-        return (statusCode >= 200 && statusCode < 300) ? true : false
+            })
     }
 }
--- MithforCryptoCharts/SwiftUI/Utilities/NetworkingManager.swift
+++ MithforCryptoCharts/SwiftUI/Utilities/NetworkingManager.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import Combine
+
+class NetworkingManager {
+    
+    static func download(url: URL) -> AnyPublisher<Data, Error> {
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap { output in
+                
+                guard let response = output.response as? HTTPURLResponse,
+                      isSuccessfull(statusCode: response.statusCode) == true
+                else {
+                    throw URLError(.badServerResponse)
+                }
+                
+                return output.data
+            }
+            .receive(on: DispatchQueue.main)
+            .eraseToAnyPublisher()
+        
+        func isSuccessfull(statusCode: Int) -> Bool {
+            return (statusCode >= 200 && statusCode < 300) ? true : false
+        }
+    }
+    
+    static func handleCompletion(completion: Subscribers.Completion<Error>) {
+        switch completion {
+        case .finished:
+            break
+        case .failure(let error):
+            print("\(#function) : \(error.localizedDescription)")
+        }
+    }
+    
+}
