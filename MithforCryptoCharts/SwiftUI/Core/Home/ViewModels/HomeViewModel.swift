455ec4b64e6416606a90e2e6804d7b70703b2ab7
Dmitriy Voronin <devoronin@gmail.com>
2024-11-19 22:56:49 +0300

    Implementing marketDataSevice subsribtion

--- MithforCryptoCharts/SwiftUI/Core/Home/ViewModels/HomeViewModel.swift
+++ MithforCryptoCharts/SwiftUI/Core/Home/ViewModels/HomeViewModel.swift
@@ -9,11 +9,13 @@ import Foundation
 import Combine
 
 class HomeViewModel: ObservableObject {
+    @Published var statistics: [StatisticModel] = []
     @Published var allCoins: [CoinModel] = []
     @Published var portfolioCoins: [CoinModel] = []
     @Published var searchText: String = ""
     
     private var coinDataService = CoinDataService()
+    private var marketDataService = MarketDataService()
     private var cancellables = Set<AnyCancellable>()
      
     init() {
@@ -31,6 +33,34 @@ class HomeViewModel: ObservableObject {
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables )
+        
+        marketDataService.$marketData
+            .map { (marketModel) -> [StatisticModel] in
+                
+                var stats: [StatisticModel] = []
+                               
+                guard let data = marketModel else { return stats }
+                
+                let marketCap = StatisticModel(title: "Market Cap",
+                                               value: data.marketCap,
+                                               percentageChange: data.marketCapChangePercentage24HUsd)
+                let volume = StatisticModel(title: "24h Volume",
+                                            value: data.volume)
+                let btcDominance = StatisticModel(title: "BTC Dominance", value: data.bitcoinDominance)
+                let portfolio = StatisticModel(title: "Portfolio Value", value: "0.00", percentageChange: 0)
+                
+                stats.append( contentsOf: [
+                    marketCap,
+                    volume,
+                    btcDominance,
+                    portfolio
+                ])
+                return stats
+            }
+            .sink { [weak self] returnedStats in
+                self?.statistics = returnedStats
+            }
+            .store(in: &cancellables)
     }
     
     private func filterCoins(text: String, coins: [CoinModel]) -> [CoinModel] {
