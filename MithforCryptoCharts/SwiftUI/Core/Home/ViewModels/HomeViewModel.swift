fc0c8f4c320bdc3e414973c40ba97936a1a6e3a0
Dmitriy Voronin <devoronin@gmail.com>
2024-11-19 16:40:33 +0300

    Add debounce for search text input process

--- MithforCryptoCharts/SwiftUI/Core/Home/ViewModels/HomeViewModel.swift
+++ MithforCryptoCharts/SwiftUI/Core/Home/ViewModels/HomeViewModel.swift
@@ -21,13 +21,15 @@ class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
+        
+        // update allCoins
         $searchText
             .combineLatest(dataService.$allCoins)
+            .debounce(for: 0.5, scheduler: DispatchQueue.main)
             .map(filterCoins)
-            .sink(receiveValue: { [weak self] (returnedCoins) in
-                guard let self = self else { return }
-                self.allCoins = returnedCoins
-            })
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
             .store(in: &cancellables )
     }
     
@@ -36,7 +38,7 @@ class HomeViewModel: ObservableObject {
         
         let lowercasedText = text.lowercased()
         
-        let filteredCoins = coins.filter{(coin) -> Bool in
+        let filteredCoins = coins.filter {(coin) -> Bool in
             return coin.name.lowercased().contains(lowercasedText) || coin.symbol.lowercased().contains(lowercasedText) ||
             coin.id.lowercased().contains(lowercasedText)
         }
