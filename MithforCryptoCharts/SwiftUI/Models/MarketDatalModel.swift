53e1415f603f5da547eea0cbc4c5ddb90663537a
Dmitriy Voronin <devoronin@gmail.com>
2024-11-19 21:53:42 +0300

    Implementing  MarketDataModel

--- MithforCryptoCharts/SwiftUI/Models/MarketDatalModel.swift
+++ MithforCryptoCharts/SwiftUI/Models/MarketDatalModel.swift
@@ -184,17 +184,25 @@ struct MarketDatalModel: Codable {
     }
     
     var marketCap: String {
-//        if let item = totalMarketCap.first(where: { (key, value) -> Bool in
-//            return key == "usd"
-//        }) {
-//            return "\(item.value)"
-//        }
-//        return ""
         
         if let item = totalMarketCap.first(where: { $0.key == "usd"}) {
             return "\(item.value)"
         }
-        return " "
+        return ""
+    }
+    
+    var volume: String {
+        if let item = totalVolume.first(where: { $0.key == "usd"}) {
+            return "\(item.value)"
+        }
+        return ""
+    }
+    
+    var bitcoinDominance: String {
+        if let item = marketCapPercentage.first(where: { $0.key == "btc"}) {
+            return item.value.asPercentString()
+        }
+        retrun ""
     }
 }
 
