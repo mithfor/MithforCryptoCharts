8eaced029db0558a5547b5eee719ed1e8de7805e
Dmitriy Voronin <devoronin@gmail.com>
2024-11-19 16:16:58 +0300

    Implementing filtering data based on search bar text using Combine

--- MithforCryptoCharts/SwiftUI/Core/Home/ViewModels/HomeViewModel.swift
+++ MithforCryptoCharts/SwiftUI/Core/Home/ViewModels/HomeViewModel.swift
@@ -21,12 +21,24 @@ class HomeViewModel: ObservableObject {
     }
     
     private func addSubscribers() {
-        dataService.$allCoins
-            .sink { [weak self] returnedCoins in
-                guard let self = self else { return }
-            
-                self.allCoins = returnedCoins
+        
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .map { (text, startingCoins) -> [CoinModel] in
+                guard !text.isEmpty else { return startingCoins }
+                
+                let lowercasedText = text.lowercased()
+                
+                let filteredCoins = startingCoins.filter{ (coin) -> Bool in
+                    return coin.name.lowercased().contains(lowercasedText) || coin.symbol.lowercased().contains(lowercasedText) ||
+                    coin.id.lowercased().contains(lowercasedText)
+                }
+                
+                return filteredCoins
             }
-            .store(in: &cancellables)
+            .sink(receiveValue: { [weak self] returnedCoins in
+                self?.allCoins = returnedCoins
+            })
+            .store(in: &cancellables )
     }
 }
