edccad1796983527cd8569148bb011713a19d308
Dmitriy Voronin <devoronin@gmail.com>
2024-11-20 10:55:38 +0300

    Add PortfolioView

---
+++ MithforCryptoCharts/SwiftUI/Core/Home/Views/PortfolioView.swift
@@ -0,0 +1,18 @@
+//
+//  PortfolioView.swift
+//  MithforCryptoCharts
+//
+//  Created by Dmitrii Voronin on 20.11.2024.
+//
+
+import SwiftUI
+
+struct PortfolioView: View {
+    var body: some View {
+        Text("PORTFOLIO VIEW")
+    }
+}
+
+#Preview {
+    PortfolioView()
+}
